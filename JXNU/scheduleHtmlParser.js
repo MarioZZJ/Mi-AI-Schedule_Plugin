62bafbbe9f283e1981289359ddc608247d29ba7b
PlumberMario <52254876+MarioZZJ@users.noreply.github.com>
2020-08-04 20:43:33 +0800

    1.0完善版本
    1.解决服务端一直无法读取的问题：原html中存在未转义的反斜杠字符\，在provider中进行转码即可。由于本插件只要求获取数据，不必正常交互。
    2.进行了相邻格课程的合并，空指针问题的存在让最终实现的方法较为拙劣，待优化。

--- JXNU/scheduleHtmlParser.js
+++ JXNU/scheduleHtmlParser.js
@@ -1,8 +1,13 @@
+function sortDay(a,b){
+    if(a.day == b.day) {
+        return a.sections[0].section - b.sections[0].section
+    }else {return a.day-b.day}
+}
 function scheduleHtmlParser(html) {
     let result = []
     let courseNteacher = []
     const $ = cheerio.load(html,{decodeEntities: false});
-    $('#_ctl1_dgStudentLesson tbody').find('tr').each(function(){
+    $('#_ctl1_dgStudentLesson tbody').find('tr').each(function(){//建立课程名-教师关系
         let course = {}
         let ctinfo = []
         $(this).find('td').each(function() {
@@ -14,14 +19,14 @@ function scheduleHtmlParser(html) {
         courseNteacher.push(course)
     })
 
-    $('#_ctl1_NewKcb table tbody').find('tr').each(function(row){
+    $('#_ctl1_NewKcb table tbody').find('tr').each(function(row){//获取课程信息
         $(this).find('td').each(function(column){
             let courseinfo = {}
             if($(this).attr('bgcolor') == "#66FFCC") {
                 let cell = $(this).find('div').html().split('<br>');
                 let cname = cell[0]
                 courseinfo.name = cname
-                courseinfo.position = cell[1]
+                courseinfo.position = cell[1].split('(')[0] == null ? cell[0].split('(')[0].trim():cell[1]
                 courseNteacher.forEach(r => {
                     if(cname == r.name) {
                         courseinfo.teacher = r.teacher
@@ -61,6 +66,57 @@ function scheduleHtmlParser(html) {
             }
         })
     })
+
+    result.sort(sortDay)
     console.info(result)
+    let len = result.length
+
+    for(let i=0;i<len-2;i++) {
+        if(result[i].name == result[i+2].name) {
+            result[i+2].sections.forEach(function(currentValue) {
+                result[i].sections.push(currentValue)
+            })
+            result[i+1].sections.forEach(function(currentValue) {
+                result[i].sections.push(currentValue)
+            })
+            result.splice(i+1,2)
+            len-=2
+        }else if(result[i].name == result[i+1].name) {
+            result[i+1].sections.forEach(function(currentValue) {
+                result[i].sections.push(currentValue)
+            })
+            result.splice(i+1,1)
+            len-=1
+        }
+        }
+
+    try{if(result[len-2].name==result[len-1].name) {
+        result[len-1].sections.forEach(function(currentValue){
+            result[len-2].sections.push(currentValue)
+        })
+        result.splice(len-1,1)
+        len-=1
+    }}catch (e) {
+       console.info(e)
+    }
+
+    // result.forEach(function(item,index,array) {
+    //     console.info(result)
+    //     if (item.name == array[index + 1].name) {
+    //         array[index + 1].sections.forEach(function (currentValue) {
+    //             item.sections.push(currentValue)
+    //         })
+    //         if (item.name == array[index + 2].name) {
+    //             array[index + 2].sections.forEach(function (currentValue) {
+    //                 item.sections.push(currentValue)
+    //             })
+    //             array.splice(index, 2)
+    //             len-=2
+    //         }else{
+    //         array.splice(index, 1)
+    //         len-=1}
+    //
+    //     }
+
     return { courseInfos: result }
 }
